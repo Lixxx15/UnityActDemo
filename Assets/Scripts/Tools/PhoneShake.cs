f0517ae55dd09e914ec5a98b51ff2d1599e9964d
Lixxx15 <lixiang23232@gmail.com>
2021-03-25 11:26:38 +0800

    LiXiang

---
+++ Assets/Scripts/Tools/PhoneShake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace Tools
+{
+    public class PhoneShake:MonoBehaviour
+    {
+        
+        public void Shake(int price)
+        {
+            p = price;
+            StartCoroutine(ShakeTimer());
+        }
+
+        private int p;
+        private IEnumerator ShakeTimer()
+        {
+            while (p > 0)
+            {
+                yield return new WaitForSeconds(0.5f);
+                p--;
+                Handheld.Vibrate();
+                Debug.Log("Shake");
+            }
+
+        }
+    }
+}
